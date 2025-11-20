43c2ea968e5652112cc2d9822c2238427ff2d60b
Jawad Hafeez <jawadhafeez171@gmail.com>
2025-11-20 17:29:02 +0530

    banner

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -16,6 +16,14 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       <main className="flex-grow">
         {children}
       </main>
+      
+      {/* Branding Banner */}
+      <div className="bg-soft-gray py-16 w-full overflow-hidden select-none flex justify-center items-center px-4">
+         <span className="font-montserrat font-extrabold text-2xl sm:text-4xl md:text-6xl uppercase text-empower-blue/20 tracking-widest text-center cursor-default break-words">
+            Encourage India IAS
+         </span>
+      </div>
+
       <Footer />
       <FloatingCallButton />
       <EnrollmentModal />
